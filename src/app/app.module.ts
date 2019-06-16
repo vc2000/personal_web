e283ce0d8ea9b9ba38eb8e273237392a9d603400
Venus Chau <vc2000@wildcats.unh.edu>
2019-06-16 14:54:10 +0800

    added form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
+//component
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
@@ -10,6 +13,8 @@ import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
 
+//services
+import {DataService} from './services/data.service';
 
 @NgModule({
   declarations: [
@@ -25,8 +30,11 @@ import { HomeComponent } from './components/home/home.component';
     AppRoutingModule,
     MatButtonModule,
     MatCheckboxModule,
+    HttpClientModule,
+    ReactiveFormsModule
+
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from '../../services/data.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  h1Style: boolean =false;
-  constructor() { }
+  users: Object;
+  h1Style: boolean = false;
+
+  constructor(
+    private data: DataService
+  ) { }
 
   ngOnInit() {
+    this.createUser();
   }
 
-  onfirstClick() {
+  createUser(){
+    this.data.getUsers().subscribe(data => {
+      this.users = data;
+      console.log(this.users)
+    }
+
+    )
+  }
+
+  changeColour(){
     this.h1Style = true;
-
   }
+
 
 }
